aada31ac010cadd61f9da8b59bb81f0141cd5956
XyzYi <xyzhang001@gmail.com>
2017-12-20 12:29:04 +1100

    Revert "js file paths"
    This reverts commit 2190b030d66873adbbe063d7966941b2081aee55.

--- public/scripts/component.js
+++ public/scripts/component.js
@@ -59,7 +59,7 @@ var CommentList = React.createClass({
     render: function(){
         var commentNodes = this.props.data.map(function (comment){
             return (
-                <Comment author={comment.author}> key={comment.id}>
+                <Comment author={comment.author}>
                 {comment.text}
                 </Comment>
             );
